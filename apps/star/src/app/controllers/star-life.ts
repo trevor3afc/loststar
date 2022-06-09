bb5c1d9f8bb1a80098bfb7f8fdee9e47f44d21d9
cosine <cosine@pc-steel>
2022-06-10 00:11:48 +0800

    June 10, 2022 12:11 AM

--- apps/star/src/app/controllers/star-life.ts
+++ apps/star/src/app/controllers/star-life.ts
@@ -1,13 +1,13 @@
 import { getLogger } from '@loststar/helper/logger';
 import { getBrowser, initBrowserInstance } from '@loststar/helper/puppeteer';
 import { benchmarkEnd, benchmarkStart, sleep } from '@loststar/utils/common';
-import { Browser } from 'puppeteer';
+import { Browser, CDPSession } from 'puppeteer';
 import { PuppeteerScreenRecorder } from 'puppeteer-screen-recorder';
 import { PassThrough } from 'stream';
 import { browserConfig } from '../constants/puppeteer';
 
 const logger = getLogger();
-
+const frames = [];
 export const starPrepare = async () => {
   logger.info('starPrepare');
   await sleep({ ms: 1000 });
@@ -19,17 +19,39 @@ export const testLife = async () => {
   });
   browser = await getBrowser();
   const page = await browser.newPage();
-  //const recorder = new PuppeteerScreenRecorder(page);
-  //const pipeStream = new PassThrough();
-  //await recorder.startStream(pipeStream);
-  //await page.goto('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
-  //await page.keyboard.press('k');
+
+  await page.goto('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+  await page.keyboard.press('k');
+  const session = await page.target().createCDPSession();
+  await session.send('Page.startScreencast');
+  session.on('Page.screencastFrame', (event) => {
+    event.data; // Base64 encoded frame
+    frames.push(event.data);
+  });
   await sleep({ ms: 20000 });
+  await session.send('Page.stopScreencast');
   await browser.close();
 };
 
 export const starMain = async () => {
   logger.info('star-life:main');
+};
+
+const playFrames = async () => {
+  let play = true;
+  const counter = 0;
+  while (play) {
+    const nextFrame = frames.shift();
+    counter++;
+    console.log({
+      counter,
+      length: frames.length,
+    });
+    play = false;
+    await sleep({
+      ms: 500,
+    });
+  }
 };
 
 export const runStarLife = async () => {
