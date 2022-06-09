baf203513bfa76f733dcb8f9e782531b628f6008
cosine <cosine@pc-steel>
2022-06-10 00:57:13 +0800

    June 10, 2022 12:57 AM

--- apps/star/src/app/controllers/initial.ts
+++ apps/star/src/app/controllers/initial.ts
@@ -2,7 +2,6 @@ import { looper, sleep } from '@loststar/utils/common';
 import { runStarLife } from './star-life';
 
 export const exeInitial = async () => {
-  process.env.FLUENTFFMPEG_COV = 'false';
   await runStarLife();
   await sleep({ ms: 1000 });
   await looper({ ms: 1000 });
