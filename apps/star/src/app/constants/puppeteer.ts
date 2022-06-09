78c1a8f45d11edf3d5343210d506b7cb7b270c28
cosine <cosine@pc-steel>
2022-06-09 22:33:53 +0800

    June 9, 2022 10:33 PM

--- apps/star/src/app/constants/puppeteer.ts
+++ apps/star/src/app/constants/puppeteer.ts
@@ -3,5 +3,9 @@ import { browserConnectionConfigs } from '@loststar/helper/puppeteer';
 export const browserConfig: browserConnectionConfigs = {
   headless: false,
   slowMo: 100,
+  defaultViewport: {
+    width: 1920,
+    height: 1080,
+  },
   args: ['--window-size=1920,1080'],
 };
