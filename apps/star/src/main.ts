07740ab9eba8de25117080dfa576744039fe798f
cosine <cosin@vgt-hk.net>
2022-06-10 09:44:04 +0800

    update

--- apps/star/src/main.ts
+++ apps/star/src/main.ts
@@ -3,5 +3,9 @@ import { exeInitial } from './app/controllers/initial';
 console.log('Hello World!');
 
 (() => {
+  console.log({
+    debug: 'env',
+    FLUENTFFMPEG_COV: process.env.FLUENTFFMPEG_COV,
+  });
   exeInitial();
 })();
