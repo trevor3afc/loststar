f0e970f68231f5a96e40911875c787fb42ebf537
cosine <cosine@pc-steel>
2022-06-19 00:44:25 +0800

    June 19, 2022 12:44 AM

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,5 +1,13 @@
+import { initLogger } from '@loststar/helper/logger';
 import { startBasic } from './app/basic';
 
+const appInitializer = async () => {
+  initLogger({
+    levels: {},
+  });
+};
+
 (async () => {
+  await appInitializer();
   await startBasic();
 })();
